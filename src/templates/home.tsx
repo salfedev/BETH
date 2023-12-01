c739ed5f3717a74eff0bd4f0721c73bbcd146b07
salfedev <sal.abuewilly@capgemini.com>
2023-12-01 17:04:30 +0000

    add static files, add homepage template

---
+++ src/templates/home.tsx
@@ -0,0 +1,19 @@
+import * as elements from "typed-html";
+
+export const HomePage = () => {
+  return (
+    <main class="grid min-h-full bg-white">
+      <div class="flex">
+        <div class="flex-none w-14 border-1 bg-[#141a3b]">
+          .
+        </div>
+        <div class="flex-auto w-64 border-content-4 bg-[#0f142e]">
+          .
+        </div>
+        <div class="flex-auto w-full border-content-4 bg-[#141a3b]">
+          .
+        </div>
+      </div>
+    </main>
+  );
+};
